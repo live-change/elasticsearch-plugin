d8fadb0f0a6b55cca8d7296e140b715576db7a01
Michał Łaszczewski <michal@laszczewski.com>
2021-10-26 10:22:50 +0200

    process env

--- index.js
+++ index.js
@@ -5,23 +5,24 @@ const searchIndex = require('./lib/searchIndex.js')
 const indexProcess = require('./lib/process.js')
 
 module.exports = function(app, services) {
+  const env = process.env
   app.connectToSearch = () => {
-    if(!app.env.SEARCH_INDEX_PREFIX) throw new Error("ElasticSearch not configured")
+    if(!env.SEARCH_INDEX_PREFIX) throw new Error("ElasticSearch not configured")
     if(app.search) return app.search
-    app.searchIndexPrefix = app.env.SEARCH_INDEX_PREFIX
-    app.search = new ElasticSearch({ node: app.env.SEARCH_URL || 'http://localhost:9200' })
+    app.searchIndexPrefix = env.SEARCH_INDEX_PREFIX
+    app.search = new ElasticSearch({ node: env.SEARCH_URL || 'http://localhost:9200' })
     //this.search.info(console.log)
     return app.search
   }
 
   app.connectToAnalytics = () => {
-    if(!app.env.ANALYTICS_INDEX_PREFIX) throw new Error("ElasticSearch analytics not configured")
+    if(!env.ANALYTICS_INDEX_PREFIX) throw new Error("ElasticSearch analytics not configured")
     if(app.analytics) return app.analytics
-    app.analytics = new AnalyticsWriter(app.env.ANALYTICS_INDEX_PREFIX)
+    app.analytics = new AnalyticsWriter(env.ANALYTICS_INDEX_PREFIX)
     return app.analytics
   }
 
-  if(app.env.SEARCH_INDEX_PREFIX) app.defaultProcessors.push(searchIndex)
-  if(app.env.SEARCH_INDEX_PREFIX) app.defaultUpdaters.push(updater)
-  if(app.env.SEARCH_INDEX_PREFIX) app.defaultUpdaters.push(indexProcess)
+  if(env.SEARCH_INDEX_PREFIX) app.defaultProcessors.push(searchIndex)
+  if(env.SEARCH_INDEX_PREFIX) app.defaultUpdaters.push(updater)
+  if(env.SEARCH_INDEX_PREFIX) app.defaultUpdaters.push(indexProcess)
 }
